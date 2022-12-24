09a0c7658b2fbac29ed0c58088122cdf9a7a98fa
unknown <abdelhalimbenoun@gmail.com>
2022-12-24 14:07:49 +0100

    jenkins debug

--- react/src/App.js
+++ react/src/App.js
@@ -1,17 +1,18 @@
 import './index.css'
-import { useEffect,useState } from 'react';
-import { BrowserRouter,Routes,Route } from "react-router-dom";
-import {Login,ErrorPage,Dashboard} from "./pages"
+import { useEffect, useState } from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Login, ErrorPage, Dashboard } from "./pages"
 import { Footer } from "./components";
 import { useDispatch, useSelector } from 'react-redux';
 import { getProjects } from './actions/projects';
 import { getUsers } from './actions/users';
 
+
 function App() {
-  const selector = useSelector((state)=>state?.auth)
+  const selector = useSelector((state) => state?.auth)
   const first = JSON.parse(localStorage.getItem('profile'))
   const [firstSelector, setfirstSelector] = useState(first)
-  const dispatch=useDispatch()
+  const dispatch = useDispatch()
   useEffect(() => {
     setfirstSelector(first)
     dispatch(getProjects())
@@ -19,18 +20,18 @@ function App() {
   }, [selector])
 
   return (
-      <BrowserRouter>
-        <div className="flex flex-col justify-between" style={{minHeight:'100vh'}}>
-      {first&&<Dashboard/>}
+    <BrowserRouter>
+      <div className="flex flex-col justify-between" style={{ minHeight: '100vh' }}>
+        {first && <Dashboard />}
         <Routes>
           {!first &&
-            (<Route path="/" element={<Login/>}/>)
+            (<Route path="/" element={<Login />} />)
           }
-          {!first &&<Route path="*" element={<ErrorPage/>}/>}
+          {!first && <Route path="*" element={<ErrorPage />} />}
         </Routes>
-        <Footer/>
-        </div>
-      </BrowserRouter>
+        <Footer />
+      </div>
+    </BrowserRouter>
   );
 }
 
