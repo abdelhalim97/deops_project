fbd0f8ec54c8bf82937d3df0663471c1faeec095
unknown <abdelhalimbenoun@gmail.com>
2022-12-24 15:12:38 +0100

    jenikns debug

--- nest/src/tasks/dto/create-user-task.dto.ts
+++
@@ -1,8 +0,0 @@
-import { IsNotEmpty } from "class-validator";
-
-export class createUerTask {
-    @IsNotEmpty()
-    email: string;
-    @IsNotEmpty()
-    password: string;
-}
--- nest/src/utils/user.types.ts
+++
@@ -1,9 +0,0 @@
-export type SignupUser = {
-    username: string
-    email: string
-    password: string;
-}
-export enum UserRole {
-    ADMIN = "admin",
-    USER = "user",
-}
